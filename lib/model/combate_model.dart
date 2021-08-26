9a606767c5d6856133c6738cfb1b1b763b9ce588
ariel <arielsam56@gmail.com>
2021-08-26 13:35:32 -0300

    update

--- lib/model/combate_model.dart
+++ lib/model/combate_model.dart
@@ -1,6 +1,8 @@
 class CombateModel{
   String nameA;
   String nameB;
+  int hitsA = 0;
+  int hitsB = 0;
   String equipeA;
   String equipeB;
   String categoria;
--- lib/view/Poomsae/PoomsaeA.dart
+++ lib/view/Poomsae/PoomsaeA.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter/widgets.dart';
 import 'package:tcc_mobile/model/combate_model.dart';
 
 class PoomsaeA extends StatefulWidget {
@@ -13,6 +14,7 @@ class PoomsaeA extends StatefulWidget {
 }
 
 class _PoomsaeAState extends State<PoomsaeA> {
+
 
   @override
   void initState(){
@@ -34,22 +36,164 @@ class _PoomsaeAState extends State<PoomsaeA> {
 
       ),
       body: Container(
-        child: Row(
+        child: Stack(
           children: [
-            Container(
-              width: MediaQuery.of(context).size.width/2,
-              color: Colors.yellow,
-              child: Column(
-                children: [
-                  Text(widget.combateModel.nameA)
-                ],
-              ),
-            ),
-            Container(
-              width: MediaQuery.of(context).size.width/2,
-              color: Colors.grey,
-            ),
+            Row(
+              children: [
+                Container(
+                  width: MediaQuery.of(context).size.width/2,
+                  color: Colors.grey,
+                  child: Column(
+                    children: [
+                      SizedBox(height: 10,),
+                      Text(widget.combateModel.nameA,
+                        textAlign: TextAlign.center,
+                        maxLines: 1,
+                        style: TextStyle(
+                            fontSize: 25
+                        ),
+                      ),
+                      SizedBox(height: 10,),
+                    ],
+                  ),
+                ),
+                Container(
+                  width: MediaQuery.of(context).size.width/2,
+                  color: Colors.greenAccent,
+                  child: Column(
+                    children: [
+                      SizedBox(height: 10,),
+                      Text(widget.combateModel.nameB,
+                        textAlign: TextAlign.center,
+                        maxLines: 1,
+                        style: TextStyle(
+                            fontSize: 25
+                        ),
+                      ),
+                      SizedBox(height: 10,),
+                    ],
+                  ),
+                ),
+              ],
+            ),//NOMES
 
+
+            Align(
+              alignment: Alignment.topCenter,
+              child: Padding(
+                padding: const EdgeInsets.only(top:50.0),
+                child: Text(
+                  'HITS',
+                  style: TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold
+                  ),
+                ),
+              ),
+            ), //HITS
+            Align(
+              alignment: Alignment.bottomLeft,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Container(
+                    width: (MediaQuery.of(context).size.width/2 * 0.7),
+                    height: (MediaQuery.of(context).size.height - 200),
+                    child: Material(
+                      color: Colors.green,
+                      child: Ink(
+                        child: InkWell(
+                          onTap: (){
+                            setState(() {
+                              widget.combateModel.hitsA++;
+                            });
+                          },
+                          child: Text(widget.combateModel.hitsA.toString(),
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 50
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    width: (MediaQuery.of(context).size.width/2 * 0.3),
+                    height: (MediaQuery.of(context).size.height - 300),
+                    child: Material(
+                      color: Colors.red,
+                      child: Ink(
+                        child: InkWell(
+                          onTap: (){
+                            setState(() {
+                              widget.combateModel.hitsA--;
+                            });
+                          },
+                          child: Text('-1',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 50
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+
+                  Container(
+                    width: (MediaQuery.of(context).size.width/2 * 0.3),
+                    height: (MediaQuery.of(context).size.height - 300),
+                    child: Material(
+                      color: Colors.red,
+                      child: Ink(
+                        child: InkWell(
+                          onTap: (){
+                            setState(() {
+                              widget.combateModel.hitsB--;
+                            });
+                          },
+                          child: Text('-1',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 50
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    width: (MediaQuery.of(context).size.width/2 * 0.7),
+                    height: (MediaQuery.of(context).size.height - 200),
+                    child: Material(
+                      color: Colors.green,
+                      child: Ink(
+                        child: InkWell(
+                          onTap: (){
+                            setState(() {
+                              widget.combateModel.hitsB++;
+                            });
+                          },
+                          child: Text(widget.combateModel.hitsB.toString(),
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 50
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+
+
+                ],),
+            ),
           ],
         ),
       ),
